9fa6ff6a3ab9195ab49e865c44d3a3c44ef568f3
小林照幸 <koba-ninkigumi@users.noreply.github.com>
2025-09-06 22:30:32 +0900

    Update ffmpeg-lgpl.rb

--- ffmpeg-lgpl.rb
+++ ffmpeg-lgpl.rb
@@ -19,6 +19,15 @@ class FfmpegLgpl < Formula
     sha256 arm64_sequoia: "902430338339abf0ae82a60268e5c5fae4ab623f01d1050b3eec8773fa928d91"
   end
 
+  bottle do
+    root_url "https://github.com/lgpl-dev/homebrew-ffmpeg-lgpl/releases/download/v8.0_1"
+    sha256 arm64_sequoia: "902430338339abf0ae82a60268e5c5fae4ab623f01d1050b3eec8773fa928d91"    
+    sha256 arm64_sonoma:  "22bbbe0eb3014af09772c29416750ab70226a6a9783545687a2979dc3235bd7a"
+    sha256 arm64_ventura: "060fe8d538f8a205a4cc0087c60797aef7bb6e0a91fcd0a0883e0b5e44dfd5c7"
+    sha256 sonoma:        ""
+    sha256 ventura:       "060fe8d538f8a205a4cc0087c60797aef7bb6e0a91fcd0a0883e0b5e44dfd5c7"
+  end
+
   livecheck do
     url "https://ffmpeg.org/download.html"
     regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
